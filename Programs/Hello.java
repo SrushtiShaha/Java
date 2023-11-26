2a3440442a834d18fd87902f34009c21eec3a4c0
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2023-11-26 20:01:37 +0530

    Java

---
+++ Programs/Hello.java
@@ -0,0 +1,17 @@
+public class Hello
+{
+	public static void main(String args[])
+	{
+		System.out.print("Hello World");
+		System.out.println(" India is My Country ");
+		System.out.print("12345");
+		System.out.println(12345);
+		System.out.println(12345 + 1);
+		
+		// Concadination
+		System.out.println(91 + " is a telephonoc code of India.");
+		System.out.println(78 + 29);
+		System.out.println("\n Hi");
+		// System.out.print(Shaha);
+	}
+}
