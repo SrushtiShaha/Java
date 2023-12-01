42edb9d6d43e9ba759afe1d4ac7b405e955b00bf
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2023-12-01 21:38:01 +0530

    Java

---
+++ Programs/Basic_Programs/Data_Types.java
@@ -0,0 +1,41 @@
+public class Data_Types
+{
+	public static void main(String args[])
+	{
+		byte bNum = 127;
+		byte bNo = -128;
+		short sNo = 12345;
+		int iNo = 1234567890;
+		long lNo = 12345;
+		float fNo = 100.123456789f;
+		double dNo = 10.1234567890123456789;
+		char Ch = 'Z';
+		boolean Val = false;
+
+		System.out.println("\n****************************************************************");
+
+		System.out.println(bNum);
+		System.out.println(bNo);
+		System.out.println(sNo);
+		System.out.println(iNo);
+		System.out.println(lNo);
+		System.out.println(fNo);
+		System.out.println(dNo);
+		System.out.println(Ch);
+		System.out.println(Val);
+
+		System.out.println("\n****************************************************************");
+
+		System.out.println("bNum Is A Byte Type Of Variable = " + bNum);
+		System.out.println("bNo Is A Byte Type Of Variable = " + bNo);
+		System.out.println("sNo Is A Byte Type Of Variable = " + sNo);
+		System.out.println("iNo Is A Byte Type Of Variable = " + iNo);
+		System.out.println("lNo Is A Byte Type Of Variable = " + lNo);
+		System.out.println("fNo Is A Byte Type Of Variable = " + fNo);
+		System.out.println("dNo Is A Byte Type Of Variable = " + dNo);
+		System.out.println("Ch Is A Byte Type Of Variable = " + Ch);
+		System.out.println("Val Is A Byte Type Of Variable = " + Val);
+
+		System.out.println("\n****************************************************************");
+	}
+}
