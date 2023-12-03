d1324677321af2917f4185c4bc62f670d607d7b2
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2023-12-03 20:33:13 +0530

    Java

---
+++ Programs/Basic_Programs/String_Concat.java
@@ -0,0 +1,17 @@
+public class String_Concat
+{
+	public static void main(String args[])
+	{
+		System.out.println("India Is My Country");
+		System.out.print("12345");
+		System.out.println(67890);
+
+		// Conditional
+
+		System.out.println(100 + 1);
+		System.out.println(91 + " Is A Telephonoc Code Of India.");
+		System.out.println("\n Srushti Shaha");
+
+		// System.out.println(Hello); - Not Alloud
+	}
+}
