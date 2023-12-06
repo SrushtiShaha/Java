bd30f993a68991b93fe016cf5596645e33f43490
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2023-12-06 19:59:08 +0530

    Java

---
+++ Programs/Conditional_Control_Statement/Simple_If_Else.java
@@ -0,0 +1,12 @@
+public class Simple_If_Else
+{
+	public static void main(String args[])
+	{
+		int Marks = 80;
+		
+		if( Marks >= 80 )
+			System.out.println("A Grade");
+		else
+			 System.out.println("B Grade");
+	}
+}
