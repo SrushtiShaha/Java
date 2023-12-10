39cca411e7affd5a38bd104c07d1e34dd1a54d93
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2023-12-10 11:17:31 +0530

    Java

---
+++ Programs/Conditional_Control_Statement/Week_Days.java
@@ -0,0 +1,36 @@
+public class Week_Days
+{
+	public static void main(String args[])
+	{
+		String Day = "Wednesday";
+		
+		if( Day == "Sunday" )
+		{
+			System.out.println("\n Idli");
+		}
+		else if( Day == "Monday" )
+		{
+			System.out.println("\n Samosa");
+		}
+		else if( Day == "Tuesday" )
+		{
+			System.out.println("\n Shira");
+		}
+		else if( Day == "Wednesday" )
+		{
+			System.out.println("\n Dosa");
+		}
+		else if( Day == "Thursday" )
+		{
+			System.out.println("\n Poha");
+		}
+		else if( Day == "Friday" )
+		{
+			System.out.println("\n Uttappa");
+		}
+		else
+		{
+			System.out.println("\n Upma");
+		}
+	}
+}
