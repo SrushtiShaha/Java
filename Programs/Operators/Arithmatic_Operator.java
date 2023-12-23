d902660982d8b2c08d4dcc88ed88cf422e2584c2
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2023-12-23 22:38:20 +0530

    Java

---
+++ Programs/Operators/Arithmatic_Operator.java
@@ -0,0 +1,29 @@
+public class Arithmatic_Operator
+{
+	public static void main(String args[])
+	{
+		int No1 = 23, No2 = 27;
+
+		System.out.println("\n******************************Addition******************************");
+		
+		System.out.println("\n Addition Of " + No1 + " And " + No2 + " Is = " + (No1 + No2) );
+
+		System.out.println("\n******************************Substraction******************************");
+
+		System.out.println("\n Substraction Of " + No1 + " And " + No2 + " Is = " + (No1 - No2) );
+
+		System.out.println("\n******************************Multiplication******************************");
+
+		System.out.println("\n Multiplication Of " + No1 + " And " + No2 + " Is = " + (No1 * No2) );
+
+		System.out.println("\n******************************Division******************************");
+
+		System.out.println("\n Division Of " + No1 + " And " + No2 + " Is = " + (No1 / No2) );
+
+		System.out.println("\n******************************Modulous******************************");
+
+		System.out.println("\n Modulous Of " + No1 + " And " + No2 + " Is = " + (No1 % No2) );
+
+		System.out.println("\n*********************************************************************");
+	}
+}
