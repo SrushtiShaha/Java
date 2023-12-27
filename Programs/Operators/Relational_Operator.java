e960dd21547754da216543089adb34d1c1a5736b
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2023-12-27 19:57:04 +0530

    Java

---
+++ Programs/Operators/Relational_Operator.java
@@ -0,0 +1,48 @@
+public class Relational_Operator
+{
+	public static void main(String args[])
+	{
+		int No1 = 23, No2 = 27;
+		boolean Ans;
+
+		System.out.println("\n No1 = " + No1 + " And No2 = " + No2);
+
+		System.out.println("\n******************************Less Than Operator******************************");
+		
+		Ans = No1 < No2;
+
+		System.out.println("\n No1 < No2 Is -> " + Ans);
+
+		System.out.println("\n******************************Greater Than Operator******************************");
+
+		Ans = No1 > No2;
+
+		System.out.println("\n No1 > No2 Is -> " + Ans);
+
+		System.out.println("\n******************************Less Than Equal To Operator******************************");
+
+		Ans = No1 <= No2;
+
+		System.out.println("\n No1 <= No2 Is -> " + Ans);
+
+		System.out.println("\n******************************Greater Than Equal To Operator******************************");
+
+		Ans = No1 >= No2;
+
+		System.out.println("\n No1 >= No2 Is -> " + Ans);
+
+		System.out.println("\n******************************Not Equal To Operator******************************");
+
+		Ans = No1 != No2;
+
+		System.out.println("\n No1 != No2 Is -> " + Ans);
+
+		System.out.println("\n******************************Equal Equal To Operator******************************");
+
+		Ans = No1 == No2;
+
+		System.out.println("\n No1 == No2 Is -> " + Ans);
+
+		System.out.println("\n*********************************************************************");
+	}
+}
