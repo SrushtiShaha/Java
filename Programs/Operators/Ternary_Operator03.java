82c892d8895f38aa86fbc92fc1e9a04c2ab26f9a
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2023-12-30 13:40:16 +0530

    Java

---
+++ Programs/Operators/Ternary_Operator03.java
@@ -0,0 +1,11 @@
+public class Ternary_Operator03
+{
+	public static void main(String args[])
+	{
+		int a = 20, b = 10;
+
+		float fAns = (a < b) ? 10.10f : 09.10f;
+
+		System.out.println(fAns); 
+	}
+}
