3e6c1f526e260dd387a927327cd79c34b97f4575
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-01-09 17:14:24 +0530

    Java

---
+++ Programs/While_Loop/Power.java
@@ -0,0 +1,12 @@
+package com.sgm.task.While_Loop;
+
+public class Power {
+	public static void main(String[] args) {
+		//int No = 5;
+		
+		System.out.println("Cube Is => " + (int)Math.pow(5,3));
+		/*
+		 * No = 5; No = No * No * No; System.out.println(No);
+		 */
+	}
+}
