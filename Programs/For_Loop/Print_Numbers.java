0d74009ac2287207178bc56903318c748649ddf8
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-01-22 11:53:18 +0530

    Java

---
+++ Programs/For_Loop/Print_Numbers.java
@@ -0,0 +1,12 @@
+package com.sgm.task.For_Loop;
+
+public class Print_Numbers {
+	public static void main(String[] args) {
+		int a = 10;
+		for(int i = 10; i <= 100; i++) {
+				System.out.print(a);
+				System.out.println("\t" + i);
+				a = a + 10;
+		}
+	}
+}
