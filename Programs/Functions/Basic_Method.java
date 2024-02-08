a4f5bde8b27d95f9354471805ba853eea79b3821
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-02-08 22:34:27 +0530

    Java

---
+++ Programs/Functions/Basic_Method.java
@@ -0,0 +1,15 @@
+package Functions;
+
+public class Basic_Method 
+{
+	public void Basic()
+	{
+		System.out.println("INDIA");
+	}
+	
+	public static void main(String[] args) 
+	{
+		Basic_Method Obj = new Basic_Method();
+		Obj.Basic();
+	}
+}
