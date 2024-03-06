01eee0a3933f085583b07072177b0bf0e6463b56
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-03-06 23:04:38 +0530

    Java

---
+++ Programs/Digit_Programs/Fibonnaci_Series.java
@@ -0,0 +1,18 @@
+package Digit_Program;
+
+public class Fibonnaci_Series 
+{
+	public static void main(String[] args) 
+	{
+		int No = 10, i = 0, Dig1 = 0, Dig2 = 1, Ans = Dig1;
+		System.out.print(Dig1 + "\n" + Dig2);
+		while( i < No ) 
+		{
+			Ans = Dig1 + Dig2;
+			System.out.print("\n" + Ans);
+			Dig1 = Dig2;
+			Dig2 = Ans;
+			i++;
+		}
+	}
+}
