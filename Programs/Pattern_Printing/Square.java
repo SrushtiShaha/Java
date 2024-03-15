56fd1aa60b5f23773e3a177f1811df8d2d0db652
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-03-15 21:46:51 +0530

    Java

---
+++ Programs/Pattern_Printing/Square.java
@@ -0,0 +1,16 @@
+package Pattern_Printing;
+
+public class Square 
+{
+	public static void main(String[] args) 
+	{
+		for(int c = 1; c <= 5; c++)
+		{
+			System.out.println();
+			for(int r = 1; r <= 5; r++)
+			{
+				System.out.print(" * ");
+			}
+		}
+	}
+}
