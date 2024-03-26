2682cb88f13395a6684ca5a03a14d13fcf86b720
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-03-26 16:32:43 +0530

    Java

---
+++ Programs/Static/Main_Of_Method_Using_Static.java
@@ -0,0 +1,11 @@
+package Static;
+
+public class Main_Of_Method_Using_Static 
+{
+	public static void main(String[] args) 
+	{
+		Method_Using_Static.Method();
+		Method_Using_Static Obj = new Method_Using_Static();
+		Obj.Method();
+	}
+}
