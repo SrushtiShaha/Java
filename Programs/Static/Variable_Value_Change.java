90ef788b76b7c860d6c2a092ae34f030a376c8d8
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-03-28 19:04:38 +0530

    Java

---
+++ Programs/Static/Variable_Value_Change.java
@@ -0,0 +1,25 @@
+package Static;
+
+public class Variable_Value_Change 
+{
+	static int c;
+	int a;
+	int b;
+	
+	public Variable_Value_Change(int a, int b)
+	{
+		this.a = a;
+		this.b = b;
+	}
+	
+	public Variable_Value_Change()
+	{
+		
+	}
+	
+	/*
+	 * public static void main(String[] args) { Variable_Value_Change Obj1 = new
+	 * Variable_Value_Change(); System.out.println(Obj1.a);
+	 * System.out.println(Obj1.b); System.out.println(c); }
+	 */
+}
