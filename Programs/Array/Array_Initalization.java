87f52169699c868ac8250e4f30bb7fbbaaa9638b
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-04-05 13:38:06 +0530

    Java

---
+++ Programs/Array/Array_Initalization.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class Array_Initalization 
+{
+	public static void main(String[] args) 
+	{
+		int Array[] = { 60, 90, 70, 35, 94 };
+		String Name[] = { "a", "b", "c", "d", "e" };
+		
+		System.out.println(Array[2]);
+		System.out.println(Name[2]);
+	}
+}
