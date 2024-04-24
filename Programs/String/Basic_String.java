245214e9e8a415f487065d1115685ae514bf240d
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-04-24 22:15:24 +0530

    Java

---
+++ Programs/String/Basic_String.java
@@ -0,0 +1,17 @@
+package String;
+
+public class Basic_String 
+{
+	static int a = 23;
+	static String s = "Srushti";
+	
+	public static void main(String[] args) 
+	{
+		System.out.println(a);
+		System.out.println(s);
+		
+		//Basic_String Obj = new Basic_String();
+		//System.out.println(Obj.a);
+		//System.out.println(Obj.s);
+	}
+}
