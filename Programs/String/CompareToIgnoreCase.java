6c5be1de88baee23b06a5d0b303a5786e7e42aeb
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-05-11 00:25:48 +0530

    Java

---
+++ Programs/String/CompareToIgnoreCase.java
@@ -0,0 +1,13 @@
+package String.Ready_Made_Functions;
+
+public class CompareToIgnoreCase 
+{
+	public static void main(String[] args) 
+	{
+		String Name = "Srushti";
+		String Surname = "Shaha";
+		
+		int Gap = Name.compareToIgnoreCase(Surname);
+		System.out.println(Gap);
+	}
+}
