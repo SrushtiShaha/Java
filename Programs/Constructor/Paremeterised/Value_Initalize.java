1244dcfb43e3fcfa3e52d66a894df4cd8471f167
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-05-19 22:45:10 +0530

    Java

---
+++ Programs/Constructor/Paremeterised/Value_Initalize.java
@@ -0,0 +1,13 @@
+package Constructor.Paremeterised;
+
+public class Value_Initalize 
+{
+	int No1 = 23, No2 = 25;
+	
+	Value_Initalize(int No1, int No2)
+	{
+		No1 = 1;
+		No2 = 2;
+		System.out.println("Constructor Value_Initalize");
+	}
+}
