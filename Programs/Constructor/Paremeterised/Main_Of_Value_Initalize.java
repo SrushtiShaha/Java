2b55d211b54315ea669675e32ed887c9ae5c47af
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-05-20 22:40:33 +0530

    Java

---
+++ Programs/Constructor/Paremeterised/Main_Of_Value_Initalize.java
@@ -0,0 +1,11 @@
+package Constructor.Paremeterised;
+
+public class Main_Of_Value_Initalize 
+{
+	public static void main(String[] args) 
+	{
+		Value_Initalize Obj = new Value_Initalize(0, 0);
+		System.out.println(Obj.No1);
+		System.out.println(Obj.No2);
+	}
+}
