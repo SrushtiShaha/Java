6f78b8f8b80d463c9d69542dc005e363e8a8d84d
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-05-23 16:00:17 +0530

    Java

---
+++ Programs/Constructor/Paremeterised/Main_Of_Using_This.java
@@ -0,0 +1,11 @@
+package Constructor.Paremeterised;
+
+public class Main_Of_Using_This 
+{
+	public static void main(String[] args) 
+	{
+		Using_This Obj = new Using_This(0, 0);
+		System.out.println(Obj.No1);
+		System.out.println(Obj.No2);
+	}
+}
