f6393a9a7dc8d8f342e1c7cc944deb69e6e2f5b5
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-05-30 21:33:41 +0530

    Java

---
+++ Programs/Constructor/Non_Paremeterised/Using_Constructor.java
@@ -0,0 +1,13 @@
+package Constructor.Non_Paremeterised;
+
+public class Using_Constructor 
+{
+	int No1 = 23, No2 = 25;
+	
+	Using_Constructor()
+	{
+		No1 = 50;
+		No2 = 60;
+		System.out.println("Constructor A");
+	}
+}
