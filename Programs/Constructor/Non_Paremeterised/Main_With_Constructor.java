b5c620d8297357e573556e5a449d8616ae401df0
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-05-31 17:39:32 +0530

    Java

---
+++ Programs/Constructor/Non_Paremeterised/Main_With_Constructor.java
@@ -0,0 +1,14 @@
+package Constructor.Non_Paremeterised;
+
+public class Main_With_Constructor 
+{
+	public static void main(String[] args) 
+	{
+		Using_Constructor Obj1 = new Using_Constructor();
+		System.out.println(Obj1.No1);
+		System.out.println(Obj1.No2);
+		Using_Constructor Obj2 = new Using_Constructor();
+		System.out.println(Obj2.No1);
+		System.out.println(Obj2.No2);
+	}
+}
