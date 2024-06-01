f8ce375623bee22639417d887f508d524beb5968
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-06-01 21:27:00 +0530

    Java

---
+++ Programs/Constructor/Non_Paremeterised/Main_Without_Constructor.java
@@ -0,0 +1,11 @@
+package Constructor.Non_Paremeterised;
+
+public class Main_Without_Constructor 
+{
+	public static void main(String[] args) 
+	{
+		Value_Initialize Obj = new Value_Initialize();
+		System.out.println(Obj.No1);
+		System.out.println(Obj.No2);
+	}
+}
