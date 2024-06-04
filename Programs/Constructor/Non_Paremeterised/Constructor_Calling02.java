1a152728b573450b0933f4f3c558651c1a07ee8b
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-06-04 19:21:51 +0530

    Java

---
+++ Programs/Constructor/Non_Paremeterised/Constructor_Calling02.java
@@ -0,0 +1,9 @@
+package Constructor.Non_Paremeterised;
+
+public class Constructor_Calling02 
+{
+	public Constructor_Calling02()
+	{
+		System.out.println("Constructor Of Constructor_Calling02");
+	}
+}
