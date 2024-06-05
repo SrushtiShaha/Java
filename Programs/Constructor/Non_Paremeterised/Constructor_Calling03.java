4eb741b7faf84aec51e77f1728c8c9cf9b8e53df
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-06-05 14:36:35 +0530

    Java

---
+++ Programs/Constructor/Non_Paremeterised/Constructor_Calling03.java
@@ -0,0 +1,9 @@
+package Constructor.Non_Paremeterised;
+
+public class Constructor_Calling03 
+{
+	public Constructor_Calling03()
+	{
+		System.out.println("Constructor Of Constructor_Calling03");
+	}
+}
