8cbd931644c15d4dcda4466c3329609132615803
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-06-06 10:38:16 +0530

    Java

---
+++ Programs/Constructor/Non_Paremeterised/Main_Of_Constructor_Calling.java
@@ -0,0 +1,15 @@
+package Constructor.Non_Paremeterised;
+
+public class Main_Of_Constructor_Calling 
+{
+	public static void main(String[] args) 
+	{
+		for(int i = 1; i <= 5; i++)
+		{
+			Constructor_Calling01 Obj1 = new Constructor_Calling01();
+			Constructor_Calling02 Obj2 = new Constructor_Calling02();
+			Constructor_Calling03 Obj3 = new Constructor_Calling03();
+			System.out.println("-------------------------------");
+		}
+	}
+}
