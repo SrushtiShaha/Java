71624ca8873408c9fce40ecf0372d81ed1aaaf46
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-06-10 17:12:19 +0530

    Java

---
+++ Programs/Super_Keyword/Child_Class.java
@@ -0,0 +1,15 @@
+package Super_Keyword;
+
+public class Child_Class extends Parent_Class
+{
+	public Child_Class()
+	{
+		// super Keyword
+		System.out.println("Child_Class Constructor.");
+	}
+	
+	public void Child()
+	{
+		System.out.println("Inside Child Class.");
+	}
+}
