9a5b165049476edafd52b7a821a1696db0d17392
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-06-11 16:31:54 +0530

    Java

---
+++ Programs/Super_Keyword/Parent_Class.java
@@ -0,0 +1,14 @@
+package Super_Keyword;
+
+public class Parent_Class 
+{
+	public Parent_Class()
+	{
+		System.out.println("Parent_Class Constructor.");
+	}
+	
+	public void Parent()
+	{
+		System.out.println("Inside Parent Class.");
+	}
+}
