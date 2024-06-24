be3f28327032dc7cac43fdbcc9e1308a97671139
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-06-24 10:34:01 +0530

    Java

---
+++ Programs/Abstraction/Abstract_Class/Program01/Child_Class.java
@@ -0,0 +1,20 @@
+package Abstraction.Abstract_Class.Program01;
+
+public class Child_Class extends Parent_Class
+{
+
+	@Override
+	public void M2() 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("M2 From Abstract_Class_Child.");
+	}
+
+	@Override
+	public void M3() 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("M3 From Abstract_Class_Child.");
+	}
+	
+}
