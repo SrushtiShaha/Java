01f8429d2c949edbc05a5b96c1cd224d2555c526
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-06-26 21:00:54 +0530

    Java

---
+++ Programs/Abstraction/Abstract_Class/Program01/Main_Class.java
@@ -0,0 +1,12 @@
+package Abstraction.Abstract_Class.Program01;
+
+public class Main_Class 
+{
+	public static void main(String[] args) 
+	{
+		Parent_Class Obj = new Child_Class();
+		Obj.M1();
+		Obj.M2();
+		Obj.M3();
+	}
+}
