984cfb4e653f1ba7c17e308f7946a49042cdd108
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-06-29 20:53:12 +0530

    Java

---
+++ Programs/Abstraction/Abstract_Class/Program01/Parent_Class.java
@@ -0,0 +1,13 @@
+package Abstraction.Abstract_Class.Program01;
+
+public abstract class Parent_Class 
+{
+	public void M1()
+	{
+		System.out.println("M1 Form Abstract_Class_Parent.");
+	}
+	
+	public abstract void M2();
+	
+	public abstract void M3();
+}
