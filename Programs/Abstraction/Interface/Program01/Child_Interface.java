9ced59c4e781a484fb63d479204f97a54e724f90
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-07-26 17:00:06 +0530

    Java

---
+++ Programs/Abstraction/Interface/Program01/Child_Interface.java
@@ -0,0 +1,22 @@
+package Abstraction.Interface.Program01;
+
+public class Child_Interface implements Parent_Interface
+{
+
+	@Override
+	public void M1() {
+		// TODO Auto-generated method stub
+		System.out.println("Child_Interface M1 Method");
+	}
+
+	@Override
+	public void M2() {
+		// TODO Auto-generated method stub
+		System.out.println("Child_Interface M2 Method");
+	}
+
+	@Override
+	public void M3() {
+		System.out.println("Child_Interface M3 Method");
+	}	
+}
