49d53412749ccb0f8de839e2543bc77cb6043e38
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-08-04 21:03:54 +0530

    Java

---
+++ Programs/Abstraction/Interface/Program02/Class_B.java
@@ -0,0 +1,21 @@
+package Abstraction.Interface.Program02;
+
+public abstract class Class_B implements Class_A 
+{
+
+	@Override
+	public void M1() {
+		// TODO Auto-generated method stub
+		System.out.println("Class B Method M1");
+	}
+
+	@Override
+	public void M2() {
+		// TODO Auto-generated method stub
+		System.out.println("Class B Method M1");
+	}
+
+	@Override
+	public abstract void M3();
+
+}
