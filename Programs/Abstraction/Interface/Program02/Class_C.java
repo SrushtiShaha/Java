3a9d32e5ef6521f9309b006c3574dea086d00c66
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-08-06 23:03:21 +0530

    Java

---
+++ Programs/Abstraction/Interface/Program02/Class_C.java
@@ -0,0 +1,12 @@
+package Abstraction.Interface.Program02;
+
+public class Class_C extends Class_B
+{
+
+	@Override
+	public void M3() {
+		// TODO Auto-generated method stub
+		System.out.println("Class C Method M3");
+	}
+
+}
