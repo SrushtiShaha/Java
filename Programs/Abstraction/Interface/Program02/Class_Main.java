28f73246d25f146d7b7ff6762d3a72ef296c8e07
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-08-08 20:12:42 +0530

    Java

---
+++ Programs/Abstraction/Interface/Program02/Class_Main.java
@@ -0,0 +1,12 @@
+package Abstraction.Interface.Program02;
+
+public class Class_Main 
+{
+	public static void main(String[] args) 
+	{
+		Class_A Obj = new Class_C();
+		Obj.M1();
+		Obj.M2();
+		Obj.M3();
+	}
+}
