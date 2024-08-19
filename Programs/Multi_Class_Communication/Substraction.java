69ee18087979bfed60000885d532be3017d1bc19
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-08-19 19:23:59 +0530

    Java

---
+++ Programs/Multi_Class_Communication/Substraction.java
@@ -0,0 +1,10 @@
+package Multi_Class_Communication;
+
+public class Substraction 
+{
+	public void Sub()
+	{
+		int No1 = 20, No2 = 2;
+		System.out.println("Substraction -> " + No1 + " + " + No2 + " = " + (No1-No2));
+	}
+}
