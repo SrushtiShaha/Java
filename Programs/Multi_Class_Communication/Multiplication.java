c13ed101265cb4c9210125ce7f4d3b5d5f505273
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-08-20 22:54:58 +0530

    Java

---
+++ Programs/Multi_Class_Communication/Multiplication.java
@@ -0,0 +1,10 @@
+package Multi_Class_Communication;
+
+public class Multiplication 
+{
+	public void Mult()
+	{
+		int No1 = 20, No2 = 2;
+		System.out.println("Multiplication -> " + No1 + " + " + No2 + " = " + (No1*No2));
+	}
+}
