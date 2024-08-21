85f4b15cd56834bf4b6145d28ca27bd12e161674
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-08-21 23:21:54 +0530

    Java

---
+++ Programs/Multi_Class_Communication/Division.java
@@ -0,0 +1,10 @@
+package Multi_Class_Communication;
+
+public class Division 
+{
+	public void Div()
+	{
+		int No1 = 20, No2 = 2;
+		System.out.println("Division -> " + No1 + " + " + No2 + " = " + (No1/No2));
+	}
+}
