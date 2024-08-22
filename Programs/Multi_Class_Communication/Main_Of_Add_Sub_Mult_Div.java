fce129104211cad10f586f0bb4dc114705745f1d
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-08-22 16:06:12 +0530

    Java

---
+++ Programs/Multi_Class_Communication/Main_Of_Add_Sub_Mult_Div.java
@@ -0,0 +1,16 @@
+package Multi_Class_Communication;
+
+public class Main_Of_Add_Sub_Mult_Div 
+{
+	public static void main(String[] args) 
+	{
+		Addition a = new Addition();
+		a.Add();
+		Substraction s = new Substraction();
+		s.Sub();
+		Multiplication m = new Multiplication();
+		m.Mult();
+		Division d = new Division();
+		d.Div();
+	}
+}
