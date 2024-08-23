b109a4d1b614cb4778742bec1bec7cf901d09d81
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-08-23 19:59:35 +0530

    Java

---
+++ Programs/Multi_Class_Communication/Function_Calling01.java
@@ -0,0 +1,14 @@
+package Multi_Class_Communication;
+
+public class Function_Calling01 
+{
+	public void M1()
+	{
+		System.out.println("M1");
+	}
+
+	public void M2()
+	{
+		System.out.println("M2");
+	}
+}
