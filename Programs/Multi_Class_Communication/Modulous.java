673861702d9024599c9010dd6337addd1eda5710
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-08-28 21:04:16 +0530

    Java

---
+++ Programs/Multi_Class_Communication/Modulous.java
@@ -0,0 +1,10 @@
+package Multi_Class_Communication;
+
+public class Modulous 
+{
+	public void Mod()
+	{
+		int No1 = 20, No2 = 2;
+		System.out.println("Reminder -> " + No1 + " + " + No2 + " = " + (No1%No2));
+	}
+}
