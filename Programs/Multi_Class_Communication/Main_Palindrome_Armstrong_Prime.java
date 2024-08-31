18bb501818e350900ff9478d0c68851774033061
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-08-31 18:45:43 +0530

    Java

---
+++ Programs/Multi_Class_Communication/Main_Palindrome_Armstrong_Prime.java
@@ -0,0 +1,14 @@
+package Multi_Class_Communication;
+
+public class Main_Palindrome_Armstrong_Prime 
+{
+	public static void main(String[] args) 
+	{
+		Palindrome_Armstrong Obj = new Palindrome_Armstrong();
+		Obj.Palindrome();
+		Obj.Armstrong();
+		
+		Prime P = new Prime();
+		P.Prm();
+	}
+}
