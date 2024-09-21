c014948714889b1b2a8efa90525e3db5e5e217b3
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-09-21 17:14:47 +0530

    Java

---
+++ Programs/Inheritance/Hirarchical_Main.java
@@ -0,0 +1,10 @@
+package Inheritance;
+
+public class Hirarchical_Main 
+{
+	public static void main(String[] args) 
+	{
+		Hirarchical_Moto Obj = new Hirarchical_Moto();
+		System.out.println(Obj.Ram);
+	}
+}
