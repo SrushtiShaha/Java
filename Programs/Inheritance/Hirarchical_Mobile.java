a839d6ae3b7386e37acc9e5381f1ab769ba51b4e
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-09-22 15:21:58 +0530

    Java

---
+++ Programs/Inheritance/Hirarchical_Mobile.java
@@ -0,0 +1,17 @@
+package Inheritance;
+
+public class Hirarchical_Mobile 
+{
+	int Storage;
+	String Processor;
+	String OS;
+	int Ram;
+	String Colour;
+	
+	public Hirarchical_Mobile()
+	{
+		Storage = 64;
+		OS = "Android";
+		Ram = 8;
+	}
+}
