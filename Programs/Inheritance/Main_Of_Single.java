40ae89b486be22b14aaff87ea939e2e9d31c0339
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-09-26 22:02:19 +0530

    Java

---
+++ Programs/Inheritance/Main_Of_Single.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class Main_Of_Single 
+{
+	public static void main(String[] args) 
+	{
+		Single_Child Obj = new Single_Child();
+		Obj.Child();
+		Obj.Parent();
+	}
+}
