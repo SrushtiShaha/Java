d2240aa3fbf139b77b2cf7da576b99d13c146a8a
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-09-28 19:40:07 +0530

    Java

---
+++ Programs/Inheritance/Multilevel_Main.java
@@ -0,0 +1,10 @@
+package Inheritance;
+
+public class Multilevel_Main 
+{
+	public static void main(String[] args) 
+	{
+		Multilevel_M20 Obj = new Multilevel_M20();
+		System.out.println(Obj.FanRpm);
+	}
+}
