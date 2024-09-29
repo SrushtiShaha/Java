ad6f13099d63bb0c2e3efd27d43fe364ce3d41ae
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-09-29 22:05:48 +0530

    Java

---
+++ Programs/Inheritance/Multilevel_Mobile.java
@@ -0,0 +1,17 @@
+package Inheritance;
+
+public class Multilevel_Mobile 
+{
+	int Storage;
+	String Processor;
+	String OS;
+	int Ram;
+	String Colour;
+	
+	public Multilevel_Mobile()
+	{
+		Storage = 64;
+		OS = "Android";
+		Ram = 8;
+	}
+}
