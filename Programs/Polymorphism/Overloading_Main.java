6c6cd4f2f426699e6edb772ea9dfebc3145e26a1
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-10-06 22:03:22 +0530

    Java

---
+++ Programs/Polymorphism/Overloading_Main.java
@@ -0,0 +1,12 @@
+package Polymorphism;
+
+public class Overloading_Main 
+{
+	public static void main(String[] args) 
+	{
+		Overloding Obj = new Overloding();
+		Obj.M1(10);
+		Obj.M1("Sru");
+		Obj.M1(5, 's');
+	}
+}
