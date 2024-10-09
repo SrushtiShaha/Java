f7e19a79601f32b4b615901d6340dc68be2838b2
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-10-09 21:46:16 +0530

    Java

---
+++ Programs/Polymorphism/Overriding_Child.java
@@ -0,0 +1,19 @@
+package Polymorphism;
+
+public class Overriding_Child extends Overriding_Parent
+{
+	public void M1()
+	{
+		System.out.println("Overriding Class M1 Method.");
+	}
+	
+	public void M2()
+	{
+		System.out.println("Overriding Class M1 Method.");
+	}
+
+	public void M3()
+	{
+		System.out.println("Overriding Class M1 Method.");
+	}
+}
