768c3a3b09ac780b2ea35785ffa99f7e14784551
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-10-11 21:26:15 +0530

    Java

---
+++ Programs/Polymorphism/Overriding_Parent.java
@@ -0,0 +1,14 @@
+package Polymorphism;
+
+public class Overriding_Parent 
+{
+	public void M1()
+	{
+		System.out.println("Overriding Class M1 Method.");
+	}
+	
+	public void M2()
+	{
+		System.out.println("Overriding Class M2 Method.");
+	}
+}
