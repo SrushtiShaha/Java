133a39658aebe0b69a8a72a24fb08d0576f91b08
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-10-18 18:32:12 +0530

    Java

---
+++ Programs/Polymorphism/Assignment/Q1_Assignment/Cat_Class.java
@@ -0,0 +1,10 @@
+package Polymorphism.Assignment.Q1_Assignment;
+
+public class Cat_Class 
+{
+	public void MakeSound()
+	{
+		System.out.println("Cat Class MakeSound Method.");
+		System.out.println("Bark");
+	}
+}
