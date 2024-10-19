2bfe4e0c52e6cf37026ffc9c018d0ccce74cbadb
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-10-19 21:24:28 +0530

    Java

---
+++ Programs/Polymorphism/Assignment/Q1_Assignment/Main_Class.java
@@ -0,0 +1,10 @@
+package Polymorphism.Assignment.Q1_Assignment;
+
+public class Main_Class 
+{
+	public static void main(String[] args) 
+	{
+		Cat_Class Obj = new Cat_Class();
+		Obj.MakeSound(); 
+	}
+}
