3459c360611580050ff33144c789ac6687e31628
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-11-03 22:55:51 +0530

    Java

---
+++ Programs/Polymorphism/Assignment/Q2_Assignment/Class_Car.java
@@ -0,0 +1,9 @@
+package Polymorphism.Assignment.Q2_Assignment;
+
+public class Class_Car extends Class_Vehicle
+{
+	public void Drive()
+	{
+		System.out.println("Repairing A Car In Car Class.");
+	}
+}
