0cecec31ca8d691d532865cf213fb6b31d9e916b
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-11-04 15:05:49 +0530

    Java

---
+++ Programs/Polymorphism/Assignment/Q2_Assignment/Class_Main.java
@@ -0,0 +1,10 @@
+package Polymorphism.Assignment.Q2_Assignment;
+
+public class Class_Main 
+{
+	public static void main(String[] args) 
+	{
+		Class_Vehicle Obj = new Class_Car();
+		Obj.Drive();
+	}
+}
