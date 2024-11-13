373eee41707834dfe1f1d28b2c0a12cd70a454c6
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-11-13 20:49:39 +0530

    Java

---
+++ Programs/Polymorphism/Assignment/Q3_Assignment/Class_Main.java
@@ -0,0 +1,10 @@
+package Polymorphism.Assignment.Q3_Assignment;
+
+public class Class_Main 
+{
+	public static void main(String[] args) 
+	{
+		Class_Shape Obj = new Class_Rectangle();
+		Obj.GetArea();
+	}
+}
