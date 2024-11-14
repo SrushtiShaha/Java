57c97ce2911f74c9c949ddbdc69180935519fda7
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-11-14 21:39:16 +0530

    Java

---
+++ Programs/Polymorphism/Assignment/Q3_Assignment/Class_Rectangle.java
@@ -0,0 +1,11 @@
+package Polymorphism.Assignment.Q3_Assignment;
+
+public class Class_Rectangle extends Class_Shape
+{
+	public void GetArea() 
+	{
+		int length = 32, width = 20;
+		System.out.println("Class_Rectangle GetArea Method.");
+		System.out.println("Area Of Rectangle -> " + (length*width));
+	}
+}
