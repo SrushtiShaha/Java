b4882f9e06fcd65470d8932c925f6d79ad91d9d9
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-11-20 20:25:43 +0530

    Java

---
+++ Programs/Polymorphism/Assignment/Q4_Assignment/Class_Employee.java
@@ -0,0 +1,16 @@
+package Polymorphism.Assignment.Q4_Assignment;
+
+public class Class_Employee 
+{
+	public void Work()
+	{
+		System.out.println("Parent - Class_Employee Work Method.");
+		System.out.println("Create A Website (Work Method).");
+	}
+	
+	public void Get_Salary()
+	{
+		System.out.println("Parent - Class_Employee Get_Salary Method.");
+		System.out.println("Salary 50000 (Get_Salary Method).");
+	}
+}
