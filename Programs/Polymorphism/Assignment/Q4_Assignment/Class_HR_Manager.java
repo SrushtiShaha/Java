feb60b1619e097bef92b991c9b456575fc731fad
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2024-11-21 20:27:40 +0530

    Java

---
+++ Programs/Polymorphism/Assignment/Q4_Assignment/Class_HR_Manager.java
@@ -0,0 +1,16 @@
+package Polymorphism.Assignment.Q4_Assignment;
+
+public class Class_HR_Manager extends Class_Employee
+{
+	public void Work()
+	{
+		System.out.println("Child - Class_HR_Manager Work Method.");
+		System.out.println("Create A Website.");
+	}
+	
+	public void Add_Employee()
+	{
+		System.out.println("Child - Class_HR_Manager Add_Employee Method.");
+		System.out.println("Added Two New Employee.");
+	}
+}
