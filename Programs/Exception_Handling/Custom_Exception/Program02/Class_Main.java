d82854307c8147ad7a4abed64f459328478641cf
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2025-04-20 21:57:19 +0530

    Java

---
+++ Programs/Exception_Handling/Custom_Exception/Program02/Class_Main.java
@@ -0,0 +1,11 @@
+package Exception_Handling.Custom_Exception.Program02;
+
+public class Class_Main 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Enter Male Age And Female Age -> ");
+		MarriageAgeValidation Obj = new MarriageAgeValidation();
+		Obj.marriageAgeValidation();
+	}
+}
