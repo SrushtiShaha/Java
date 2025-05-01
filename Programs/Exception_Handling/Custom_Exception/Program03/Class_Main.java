baff1d2483adf795de3d920add4174277ba32ad8
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2025-05-01 19:31:34 +0530

    Java

---
+++ Programs/Exception_Handling/Custom_Exception/Program03/Class_Main.java
@@ -0,0 +1,11 @@
+package Exception_Handling.Custom_Exception.Program03;
+
+public class Class_Main 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Enter Your Age -> ");
+		VotingAgeValidation Obj = new VotingAgeValidation();
+		Obj.votingAgeValidation();
+	}
+}
