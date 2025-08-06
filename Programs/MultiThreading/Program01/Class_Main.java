b25475612cf2e2f3d82fe9cd84fdd37527df4ef5
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2025-08-07 01:48:27 +0800

    Java

---
+++ Programs/MultiThreading/Program01/Class_Main.java
@@ -0,0 +1,15 @@
+package MultiThreading.Program01;
+
+public class Class_Main 
+{
+	public static void main(String[] args) 
+	{
+		Print_Numbers Obj1 = new Print_Numbers();
+		Print_Numbers Obj2 = new Print_Numbers();
+		Print_Numbers Obj3 = new Print_Numbers();
+		
+		Obj1.start();
+		Obj2.start();
+		Obj3.start();
+	}
+}
