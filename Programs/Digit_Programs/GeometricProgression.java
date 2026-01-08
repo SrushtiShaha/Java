b9ac23d433ff0c9b14fd7b61cb02d5a9d1704c5a
Srushti Sudhir Shaha <95905998+SrushtiShaha@users.noreply.github.com>
2026-01-08 23:46:37 +0530

    Java

---
+++ Programs/Digit_Programs/GeometricProgression.java
@@ -0,0 +1,24 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class GeometricProgression {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int fno, ser, ra;
+        try {
+            System.out.print("Enter 1st number : ");
+            fno = sc.nextInt();
+            System.out.print("Enter common ratio : ");
+            ra = sc.nextInt();
+            System.out.print("Enter number of terms : ");
+            ser = sc.nextInt();
+        } catch(InputMismatchException e) {
+            System.err.println("Invalid input. \nEnter only number.");
+            return;
+        }
+        for(int i = 1;  i <= ser; i++) {
+            System.out.print(fno + " ");
+            fno = fno * ra;
+        }
+    }
+}
