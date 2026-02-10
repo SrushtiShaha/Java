577f47c84d640a42334f18091a64da9fef7b42a5
Srushti Sudhir Shaha <95905998+SrushtiShaha@users.noreply.github.com>
2026-02-10 16:01:19 +0530

    Java

---
+++ Programs/Digit_Programs/Array/MergeSort.java
@@ -0,0 +1,27 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size, max = 0, index = 0, temp;
+        try {
+            System.out.print("Enter size of array : ");
+            size = sc.nextInt();
+        } catch(InputMismatchException e) {
+            System.out.println("Invalid input. \nEnter only number.");
+            return;
+        }
+
+        int[] arr = new int[size];
+        try {
+            for(int i = 0; i < size; i++) {
+                System.out.print("Enter element number " + (i+1) + " : ");
+                arr[i] = sc.nextInt();
+            }
+        } catch(InputMismatchException e) {
+            System.out.println("Invalid input. \nEnter only number.");
+            return;
+        }
+    }
+}
